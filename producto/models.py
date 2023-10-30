55f5fef7fa3dcdfc717a15e5f13ef823551d72ce
Andrea Alexandra <nm0753032023@unab.edu.sv>
2023-10-30 16:45:09 -0600

    Primer Despliegue

---
+++ producto/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Producto(models.Model):
+    id = models.AutoField(primary_key = True, auto_created = True)
+    nombre = models.TextField(null = False, max_length = 50)
+    descripcion = models.TextField(null = False, max_length = 200)
+    existencias = models.IntegerField(null = False)   
+    precio = models.DecimalField(null = False, decimal_places = 2, max_digits = 9)
