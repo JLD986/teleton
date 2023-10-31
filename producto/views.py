e26dd4d7b0ba041ef5ddbafb2eedbc5a9277d8d1
andrea-alexa <126368756+andrea-alexa@users.noreply.github.com>
2023-10-30 22:50:12 -0600

    Update views.py
    apartado para las vistas de las paginas

--- producto/views.py
+++ producto/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from .models import Producto
+from .forms import ProductoForm
 
+#existen dos tipos de parametros que se usan en las peticiones http
+# 1- Request
+# 2- Response
 # Create your views here.
+
+def inicio(request):
+    return render(request, 'pages/inicio.html')
+
+def listado_productos(request):
+    #primero creamos una variable para almacenar los prodcutos
+    productos = Producto.objects.all()
+    return render(request, 'producto/productos.html', { 'productos': productos })
+
+def crear_producto(request):
+    formulario = ProductoForm(request.POST or None)
+    
+    if formulario.is_valid():
+        #producto = formulario.save(commit=False)
+        formulario.save()
+        return redirect('productos')
+    
+    return render(request, 'producto/crear.html', {'formulario':formulario})
+    
+def editar_producto(request, id):
+    producto = Producto.objects.get(id = id)
+    
+    formulario = ProductoForm(request.POST or None, instance=producto)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('productos')
+    
+    return render(request, 'producto/editar.html', {'formulario': formulario})
+
+def eliminar_producto(request, id):
+    producto = Producto.objects.get(id=id)
+    producto.delete()
+    return redirect('productos')
