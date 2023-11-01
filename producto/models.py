708f0fb39f1e4c16b34ba8f54a4e616bbc89edd6
andrea-alexa <126368756+andrea-alexa@users.noreply.github.com>
2023-10-31 18:01:31 -0600

    Update models.py

--- producto/models.py
+++ producto/models.py
@@ -5,5 +5,5 @@ class Producto(models.Model):
     id = models.AutoField(primary_key = True, auto_created = True)
     nombre = models.TextField(null = False, max_length = 50, verbose_name="Nombre Producto")
     descripcion = models.TextField(null = False, max_length = 200, verbose_name="Descripcion Producto")
-    existencias = models.IntegerField(null = False, verbose_name="Existencias Producto")   
+    existencias = models.IntegerField(null = False, verbose_name="Existencias Producto")
     precio = models.DecimalField(null = False, decimal_places = 2, max_digits = 9, verbose_name="Precio Producto")
